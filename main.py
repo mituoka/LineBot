91d7de08ddbfed05fffe56024a688ca865cd1921
光岡知洋 <mitsuokatomohiro@MacPro.local>
2021-05-05 11:40:52 +0900

    メッセージ更新

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ line_bot_api=LineBotApi(CHANNEL_ACCESS_TOKEN)
 
 def main():
     USER_ID=info['USER_ID']
-    messages=TextSendMessage(text='おはよう \n 大好きだよハート♥️')
+    messages=TextSendMessage(text='おーい！起きて！朝だよ❤️ \n 今日も1日お仕事頑張ってね❤　\n　大好きだよ♥️ちゅ❤️')
     line_bot_api.push_message(USER_ID,messages=messages)
 
 if __name__ == "__main__":
